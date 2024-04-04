bb6e5925e2992c8dc332ba7fae9c8d0f8545e893
dannyfadli <danyfdly95@gmail.com>
2024-04-04 11:02:29 +0700

    menambahkan do while untuk menjakan tgs hingga  yg di perintahkan selesai
    menambahkan ke layar element array yang ingin di cari

--- AlgorithmaSearch_0156/AlgorithmaSearch_0156.cpp
+++ AlgorithmaSearch_0156/AlgorithmaSearch_0156.cpp
@@ -35,6 +35,15 @@ void LinearSearch()
     char ch;
     int ctr; //Number of comparisons
     int item;
+
+    do
+    {
+        // Accept the number to be searched 
+        cout << "\nEnter the elemet you want to search: "; //steps 1
+        cin >> item;
+
+
+    }
 }
 
 int main()
